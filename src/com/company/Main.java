8185724799f8e28049529c4545e8e30bb2178d35
Scott Xu <fgsoap@msn.cn>
2016-07-25 22:06:23 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// write your code here
+         /*
+         System.out.println("Hello World!");
+        StringBuilder builder = new StringBuilder();
+        String myString = new String("Bye!");
+        builder.append("Hi!");
+        builder.append("Nice to meet you!");
+        builder.append(myString);
+        String cString = builder.toString();
+        System.out.println(cString);
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.next();
+        System.out.print("How old are you? ");
+        int age = in.nextInt();
+        System.out.println("Hello, " + name + ". Next year, you'll be " + (age + 1));
+
+        double x = 1000.0/3.0;
+        System.out.print(x);
+        System.out.printf("%8.2f",x);
+        */
+        System.out.printf("%Tc", new Date());
+        
+
+
+    }
+}
