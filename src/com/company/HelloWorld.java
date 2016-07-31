f329e43317b930135b9557f540a254a86b214f19
Scott Xu <fgsoap@msn.cn>
2016-07-31 09:36:31 +0800

    20170731

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Date;
+
+/**
+ * Created by scottxu on 7/31/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.printf("%Tc", new Date());
+    }
+    }
